09b08432684c51e87cff98f1649f17df25d86b40
Анастасия <a.yurshenaite@g.nsu.ru>
2024-05-02 13:13:32 +0700

    Выполнила задание №9

--- working_with_files/ex_9/task_9.py
+++ working_with_files/ex_9/task_9.py
@@ -1,15 +1,19 @@
 import os
 
-# TODO: ДЗ
-
-chars = ['a\n', 'b\n', 'c\n']
 
 directory_name = 'simple'
 file_name = 'output.txt'
 
 os.mkdir(directory_name)  # создание директории
-
 output_file_path = os.path.join(directory_name, file_name)
 
+total = []
+with open('input.txt', 'r', encoding='UTF-8') as file_r:
+    content = file_r.readlines()
+
+for i in range(len(content)):
+    if i % 2 == 1:
+        total.append(content[i])
+
 with open(output_file_path, 'w', encoding='UTF-8') as file_w:
-    file_w.writelines(chars)
+    file_w.writelines(total)
